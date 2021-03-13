821a2c75e99e6357a6f151ffeec584f6957c0512
Arthur <62344195+9thmassive@users.noreply.github.com>
2021-03-14 00:02:57 +0400

    Create oop.js

---
+++ oop.js
@@ -0,0 +1,19 @@
+class Car{
+ constructor(speed,horsPower){
+  this.speed = speed
+  this.horsPower = horsPower 
+ }
+ 
+ speedTo100(){
+  	return this.speed/this.horsPower
+ }
+
+}
+class BMW extends Car{
+	speedTo100(){
+  	return   super.speedTo100()+'Good Restul'
+  }
+}
+
+let BMW_X5 = new BMW(100,400)
+alert(BMW_X5.speedTo100())
